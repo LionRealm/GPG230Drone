dc7fb4dfe8491cd318bd7903f5da04ca45a0e7be
Lachlan  James <ljames17@sae.au>
2016-08-24 13:18:27 +1000

    A bunch of stuff - LJ
    Audio build fixes
    
    Stuffs
    
    no message
    
    Hopefully now able to push

---
+++ Source/Dron/Dummy.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Dron.h"
+#include "Dummy.h"
+
+Dummy::Dummy()
+{
+}
+
+Dummy::~Dummy()
+{
+}
---
+++ Source/Dron/Dummy.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class DRON_API Dummy
+{
+public:
+	Dummy();
+	~Dummy();
+};
